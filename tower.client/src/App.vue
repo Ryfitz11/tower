e6cfb820a9ee6b322d153fac8e2506d2f3c04f55
Ryan Fitzgerald <ryfitz@ymail.com>
2021-11-24 07:54:03 -0700

    title

--- tower.client/src/App.vue
+++ tower.client/src/App.vue
@@ -7,7 +7,7 @@
   </main>
   <footer>
     <div class="bg-dark text-light text-center p-4">
-      Made with 💖 by CodeWorks
+      Made by Ryan Fitzgerald
     </div>
   </footer>
 </template>
@@ -26,5 +26,4 @@ export default {
 </script>
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
 </style>
